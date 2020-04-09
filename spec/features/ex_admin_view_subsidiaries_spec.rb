7b90bdf7388fce659f5aac1235047ce1bafc93bc
Will Radi <williamradi@gmail.com>
2020-04-08 23:56:07 -0300

    Correção feature 'Administrator view subsidiaries'

--- spec/features/ex_admin_view_subsidiaries_spec.rb
+++ spec/features/ex_admin_view_subsidiaries_spec.rb
@@ -8,12 +8,8 @@ feature 'Administrator view subsidiaries' do
     visit root_path
     click_on 'Filiais'
 
-    expect(page).to have_content('Nome: São Paulo')
-    expect(page).to have_content('CNPJ: 123456')
-    expect(page).to have_content('Endereço: Avenida Paulista, 123')
-    expect(page).to have_content('Nome: Curitiba')
-    expect(page).to have_content('CNPJ: 654321')
-    expect(page).to have_content('Endereço: Avenida Mal. Floriano Peixoto, 123')
+    expect(page).to have_content('São Paulo')
+    expect(page).to have_content('Curitiba')
   end
 
   scenario 'and there is no subsidiaries' do
@@ -22,5 +18,20 @@ feature 'Administrator view subsidiaries' do
 
     expect(Subsidiary.count).to eq 0
     expect(page).to have_content('Nenhum registro encontrado')
+  end
+
+  scenario 'and view details' do
+    sp = Subsidiary.create!(name: 'São Paulo', cnpj: '123456', address: 'Avenida Paulista, 123')
+    ctba = Subsidiary.create!(name: 'Curitiba', cnpj: '654321', address: 'Avenida Mal. Floriano Peixoto, 123')
+
+    visit root_path
+    click_on 'Filiais'
+    click_on 'São Paulo'
+
+    expect(current_path).to eq subsidiary_path(sp.id)
+    expect(page).to have_content('São Paulo')
+    expect(page).to have_content('CNPJ: 123456')
+    expect(page).to have_content('Endereço: Avenida Paulista, 123')
+    expect(page).not_to have_content('Curitiba')
   end
 end
