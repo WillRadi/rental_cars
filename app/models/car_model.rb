a3d2473b6f6eeafa6ae0fc98cacb87615c6ea4f3
Will Radi <williamradi@gmail.com>
2020-04-19 22:13:36 -0300

    Unitário CarModel

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -2,6 +2,8 @@ class CarModel < ApplicationRecord
   belongs_to :manufacturer
   belongs_to :car_category
 
+  validates :name, presence: true
+
   def manufacturer_and_name
     "#{manufacturer.name} #{name}"
   end
--- spec/models/car_model_spec.rb
+++ spec/models/car_model_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CarModel, type: :model do
+  it 'should validate presence' do
+    car_model = CarModel.new(name: '')
+    car_model.valid?
+
+    expect(car_model.errors[:name]).to include 'não pode ficar em branco'
+  end
 end
