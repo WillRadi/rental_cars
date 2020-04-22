19fe24b7ad8f408a80580ed447b979df492a9e88
Will Radi <williamradi@gmail.com>
2020-04-22 00:03:29 -0300

    Unitários - teste de data passando (feio)

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,22 @@
 class Rental < ApplicationRecord
   belongs_to :client
   belongs_to :car_category
+
+  validate :end_date_must_be_greater_than_start_date
+  validate :start_date_is_not_past
+  validate :end_date_is_not_past
+
+  private
+
+  def end_date_must_be_greater_than_start_date
+    errors.add(:end_date, 'Data final deve ser maior do que data inicial') unless self.end_date > self.start_date
+  end
+
+  def start_date_is_not_past
+    errors.add(:start_date, 'Data inicial não pode ser menor do que o dia corrente') unless self.start_date >= Date.today
+  end
+
+  def end_date_is_not_past
+    errors.add(:end_date, 'Data final deve ser maior do que o dia corrente') unless self.end_date > Date.today
+  end
 end
--- spec/models/rental_spec.rb
+++ spec/models/rental_spec.rb
@@ -1,4 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Rental, type: :model do
+  context 'validation' do
+    xit 'should validate start date presence' do
+      rental = Rental.new(start_date: '', end_date: Date.today)
+      rental.valid?
+
+      expect(rental.errors[:start_date]).to include 'não pode ficar em branco'
+    end
+    it 'should validate end date is greater than start date' do
+      rental = Rental.new(start_date: '21/04/2020', end_date: '20/04/2020')
+      rental.valid?
+
+      expect(rental.errors[:end_date]).to include 'Data final deve ser maior do que data inicial'
+    end
+
+    it 'should validate end date is not equal to start date' do
+      rental = Rental.new(start_date: '21/04/2020', end_date: '21/04/2020')
+      rental.valid?
+
+      expect(rental.errors[:end_date]).to include 'Data final deve ser maior do que data inicial'
+    end
+
+    it 'should validate start date is not in the past' do
+      rental = Rental.new(start_date: Date.yesterday, end_date: Date.yesterday)
+      rental.valid?
+
+      expect(rental.errors[:start_date]).to include 'Data inicial não pode ser menor do que o dia corrente'
+    end
+
+    it 'should validate end date is not today' do
+      rental = Rental.new(end_date: Date.today, start_date: Date.today)
+      rental.valid?
+
+      expect(rental.errors[:end_date]).to include 'Data final deve ser maior do que o dia corrente'
+    end
+
+    it 'should validate end date is not past' do
+      rental = Rental.new(end_date: Date.yesterday, start_date: Date.today)
+      rental.valid?
+
+      expect(rental.errors[:end_date]).to include 'Data final deve ser maior do que o dia corrente'
+    end
+  end
 end
